da2a566af19e2503709613375e49910728611a8b
Rafael Evangelista <rafael.evans@live.com>
2021-02-22 19:09:24 -0300

    [MN-99] Send to matera the header hash and body to pay billet

--- lib/m2y_matera/modules/matera_payment.rb
+++ lib/m2y_matera/modules/matera_payment.rb
@@ -13,5 +13,23 @@ module M2yMatera
       return response  if response['data'].nil?
       MateraModel.new(response['data'])
     end
+
+    def payment(matera_body)
+      matera_header_hash = [
+        matera_body[:sender][:account][:branch],
+        matera_body[:sender][:account][:accountId],
+        matera_body[:sender][:client][:taxId],
+        matera_body[:paymentInfo][:transactionType],
+        matera_body[:totalAmount].to_i,
+        matera_body[:sender][:account][:mobilePhone][:phoneNumber],
+        matera_body[:totalAmount].to_i,
+      ].join('')
+
+      response = @request.post( @url + PAYMENT_PATH,
+                                matera_body, matera_header_hash)
+
+      return response if response['data'].nil?
+      MateraModel.new(response['data'])
+    end
   end
 end
