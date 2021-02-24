96c3730ab1270dd2cd3bc0f5ccfee778dcce5f58
Rafael Evangelista <rafael.evans@live.com>
2021-02-24 13:41:49 -0300

    [MN-115] List all carriers by area and country code

---
+++ lib/m2y_matera/modules/matera_recharge.rb
@@ -0,0 +1,17 @@
+module M2yMatera
+  class MateraRecharge < MateraModule
+
+    def initialize(access_key, secret_key, env)
+      startModule(access_key, secret_key, env)
+    end
+
+    def confirmDealers(country_code, area_code)
+      response = @request.get( @url + COUNTRY_PATH + country_code +
+                               AREA_CODE_PATH + area_code + CARRIERS_PATH, '')
+
+      carriers = MateraModel.new(response)
+      carriers.statusCode = response['data'].nil? ? 400 : 200
+      carriers
+    end
+  end
+end
