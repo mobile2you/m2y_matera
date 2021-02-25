cbe166c6553ea03ff9e988cbd6d8c4540231f2c6
Rafael Evangelista <rafael.evans@live.com>
2021-02-25 10:55:46 -0300

    [MN-115] List every amount using carrier id as parameter

--- lib/m2y_matera/modules/matera_recharge.rb
+++ lib/m2y_matera/modules/matera_recharge.rb
@@ -13,5 +13,15 @@ module M2yMatera
       carriers.statusCode = response['data'].nil? ? 400 : 200
       carriers
     end
+
+    def newRechargeOrder(country_code, area_code, carrier_id)
+      response = @request.get( @url + COUNTRY_PATH + country_code +
+                               AREA_CODE_PATH + area_code + CARRIERS_PATH.singularize +
+                               '/'+ carrier_id + VALUE_PATH, '')
+
+      carriers = MateraModel.new(response)
+      carriers.statusCode = response['data'].nil? ? 400 : 200
+      carriers
+    end
   end
 end
