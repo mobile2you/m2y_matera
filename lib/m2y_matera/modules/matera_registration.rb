b9ca429c6c670b3d90c7c219852a5f91f30de575
Guillaume <guillaume.zeldine@mobile2you.com.br>
2021-03-12 15:32:20 -0300

    Updated body to be send to matera

--- lib/m2y_matera/modules/matera_registration.rb
+++ lib/m2y_matera/modules/matera_registration.rb
@@ -1,92 +1,210 @@
+# frozen_string_literal: true
+
 module M2yMatera
   class MateraRegistration < MateraModule
     def initialize(access_key, secret_key, env)
       startModule(access_key, secret_key, env)
     end
 
-    def createRegistration(body,_version = 1)
+    def createRegistrationPj(body, _version = 1)
       # fix cdt_params
-      address = body[:address]
-      matera_body = {}
-      matera_body[:externalIdentifier] = rand(1..9999)
-      matera_body[:sharedAccount] = false
-      matera_body[:clientType] = 'Person'
-      matera_body[:occupation] = 1
-      matera_body[:maritalStatus] = 'SEPARATED'
-      matera_body[:businessLine] = 1
-      matera_body[:gender] = 'M'
-      matera_body[:rg] = {
-        number: "222836933",
-        issueDate: "10-10-2012",
-        issuer: "br",
-        state: "SSP"
-      }
-      matera_body[:client] = {
-        name: body[:name],
-        email: body[:email],
-        documents: body[:documents],
-        socialName: body[:legalName] || body[:name],
-        taxIdentifier: {
-          country: 'BRA',
-          taxId: body[:document]
-        },
-        mobilePhone: {
-          country: 'BRA',
-          phoneNumber: body[:phone][:areaCode] + body[:phone][:number]
-        },
-        mailAddress: {
-          logradouro: address[:street],
-          numero: address[:number],
-          cidade: address[:city],
-          estado: address[:federativeUnit],
-          cep: address[:zipCode],
-          pais: address[:country],
-          bairro: address[:neighborhood]
-        }
-      }
-      matera_body[:billingAddress] = {
-        logradouro: address[:street],
-        numero: address[:number],
-        cidade: address[:city],
-        estado: address[:federativeUnit],
-        cep: address[:zipCode],
-        pais: address[:country],
-        bairro: address[:neighborhood]
-      }
-      matera_body[:AdditionalDetailsPerson] = {
-        mother: body[:motherName],
-        birthDate: body[:birthDate],
-        birthCity: address[:city],
-        birthState: address[:federativeUnit],
-        birthCountry: address[:country],
-        legalResponsible: {
+      # matera_body = {}
+      # matera_body[:externalIdentifier] = rand(1..9999) # required
+      # matera_body[:sharedAccount] = false
+      # matera_body[:clientType] = 'Corporate'
+      # # matera_body[:occupation] = 1
+      # # matera_body[:maritalStatus] = 'SEPARATED'
+      # # matera_body[:businessLine] = 1
+      # # matera_body[:gender] = 'M'
+      # # matera_body[:rg] = {
+      #   # number: '222836933',
+      #   # issueDate: '10-10-2012',
+      #   # issuer: 'br',
+      #   # state: 'SSP'
+      # # }
+      # matera_body[:client] = {
+      #   name: body[:name], # required
+      #   email: body[:email], # required
+      #   # documents: body[:documents],
+      #   # socialName: body[:legalName] || body[:name],
+      #   taxIdentifier: { # required
+      #     country: 'BRA',
+      #     taxId: body[:document]
+      #   },
+      #   mobilePhone: { # required
+      #     country: 'BRA',
+      #     phoneNumber: body[:phone][:areaCode] + body[:phone][:number]
+      #   },
+      #   mailAddress: { # required
+      #     logradouro: address[:street],
+      #     numero: address[:number],
+      #     cidade: address[:city],
+      #     estado: address[:federativeUnit],
+      #     cep: address[:zipCode],
+      #     pais: address[:country],
+      #     bairro: address[:neighborhood]
+      #   }
+      # }
+      # matera_body[:billingAddress] = {
+      #   logradouro: address[:street],
+      #   numero: address[:number],
+      #   cidade: address[:city],
+      #   estado: address[:federativeUnit],
+      #   cep: address[:zipCode],
+      #   pais: address[:country],
+      #   bairro: address[:neighborhood]
+      # }
+      # matera_body[:AdditionalDetailsPerson] = {
+      #   mother: body[:motherName],
+      #   birthDate: body[:birthDate],
+      #   birthCity: address[:city],
+      #   birthState: address[:federativeUnit],
+      #   birthCountry: address[:country],
+      #   legalResponsible: {
+      #     name: body[:name],
+      #     email: body[:email],
+      #     documents: body[:documents],
+      #     socialName: body[:legalName] || body[:name],
+      #     taxIdentifier: {
+      #       country: 'BRA',
+      #       taxId: body[:document]
+      #     },
+      #     mobilePhone: {
+      #       country: 'BRA',
+      #       phoneNumber: body[:phone][:areaCode] + body[:phone][:number]
+      #     },
+      #     mailAddress: {
+      #       logradouro: address[:street],
+      #       numero: address[:number],
+      #       cidade: address[:city],
+      #       estado: address[:federativeUnit],
+      #       cep: address[:zipCode],
+      #       pais: address[:country],
+      #       bairro: address[:neighborhood]
+      #     }
+      #   }
+      # }
+
+      address = body[:mainAddress]
+      partners = body[:partners][:individuals]
+
+      matera_body = {
+        externalIdentifier: rand(1..9999),
+        sharedAccount: false,
+        client: {
           name: body[:name],
-          email: body[:email],
-          documents: body[:documents],
-          socialName: body[:legalName] || body[:name],
+          socialName: body[:legalName],
           taxIdentifier: {
-            country: 'BRA',
-            taxId: body[:document]
+            taxId: body[:document],
+            country: 'BRA'
           },
           mobilePhone: {
             country: 'BRA',
             phoneNumber: body[:phone][:areaCode] + body[:phone][:number]
           },
-          mailAddress: {
-            logradouro: address[:street],
-            numero: address[:number],
-            cidade: address[:city],
-            estado: address[:federativeUnit],
-            cep: address[:zipCode],
-            pais: address[:country],
-            bairro: address[:neighborhood]
-          }
+          email: body[:email]
+        },
+        additionalDetailsCorporate: {
+          companyName: body[:name],
+          businessLine: '47',
+          establishmentForm: '1',
+          establishmentDate: body[:dateEstablishment],
+          representatives: [],
+          monthlyIncome: 0
         }
       }
+
+
+      partners.each do |partner|
+        partner_address = partner[:mainAddress]
+        partner = {
+          name: partner[:name],
+          taxIdentifier: {
+            taxId: partner[:nationalRegistration],
+            country: 'BRA'
+          },
+          mobilePhone: {
+            country: 'BRA',
+            phoneNumber: partner[:mainPhone][:area] + partner[:mainPhone][:number]
+          },
+          email: body[:email],
+          mother: partner[:moterName],
+          birthDate: partner[:dateBirth],
+          mailAddress: {
+            logradouro: partner_address[:street],
+            numero: partner_address[:number],
+            cidade: partner_address[:city],
+            estado: partner_address[:federativeUnit],
+            cep: partner_address[:zipCode],
+            pais: partner_address[:country],
+            bairro: partner_address[:neighborhood]
+          }
+        }
+        matera_body[:additionalDetailsCorporate][:representatives] << partner
+      end
+
       puts matera_body
 
       response = @request.post(@url + ACCOUNT_PATH, matera_body, [matera_body[:externalIdentifier], matera_body[:client][:taxIdentifier][:taxId]].join(''))
+
       account = MateraModel.new(response)
+
+      puts account
+
+      if account&.data
+        account.data['registration_id'] = account.data['account']['accountId']
+        account.data['id'] = matera_body[:externalIdentifier]
+        account.data['account_id'] = matera_body[:externalIdentifier]
+        account.data['person_id'] = account.data['accountHolderId']
+      end
+      account
+    end
+
+    def createRegistrationPf(body, _version = 1)
+      address = body[:address]
+
+      matera_body = {
+        externalIdentifier: rand(1..9999),
+        sharedAccount: false,
+        clientType: 'PERSON',
+        accountType: 'UNLIMITED_ORDINARY',
+        client: {
+          name: body[:name],
+          birthDate: body[:birthDate],
+          mother: body[:motherName],
+          taxIdentifier: {
+            taxId: body[:document],
+            country: 'BRA'
+          },
+          mobilePhone: {
+            country: 'BRA',
+            phoneNumber: body[:phone][:areaCode] + body[:phone][:number]
+          },
+          email: body[:email]
+        },
+        billingAddress: {
+          logradouro: address[:street],
+          numero: address[:number],
+          complemento: address[:complement],
+          cidade: address[:city],
+          estado: address[:federativeUnit],
+          cep: address[:zipCode],
+          pais: address[:country],
+          bairro: address[:neighborhood]
+        },
+        additionalDetailsPerson: {
+          birthDate: body[:birthDate],
+          mother: body[:motherName]
+        }
+      }
+
+      puts matera_body
+
+      response = @request.post(@url + ACCOUNT_PATH, matera_body, [matera_body[:externalIdentifier], matera_body[:client][:taxIdentifier][:taxId]].join(''))
+
+      account = MateraModel.new(response)
+
+      puts account
 
       if account&.data
         account.data['registration_id'] = account.data['account']['accountId']
