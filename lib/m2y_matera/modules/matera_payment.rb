070334b7b30ee8eb36b035ffd7861a294bf205c3
Guillaume <guillaume.zeldine@mobile2you.com.br>
2021-03-25 17:25:25 -0300

    Endpoint to pay a boleto

--- lib/m2y_matera/modules/matera_payment.rb
+++ lib/m2y_matera/modules/matera_payment.rb
@@ -12,5 +12,32 @@ module M2yMatera
 
       MateraModel.new(response['data'] || response['error'])
     end
+    
+    def payment(account_id, matera_params)
+      matera_body = {
+        totalAmount: matera_params[:amount].to_i,
+        mediatorFee: 0,
+        currency: 'BRL',
+        externalIdentifier: rand(1..9999),
+        withdrawInfo: {
+          withdrawType: 'Boleto',
+          boleto: {
+            interestAmount: matera_params[:interest],
+            fineAmount: matera_params[:fine],
+            documentNumber: matera_params[:document],
+            typeableLine: matera_params[:linhaDigitavel],
+            beneficiaryTaxIdentifier: matera_params[:taxIdentifier],
+            dueDate: matera_params[:dueDate],
+            discount: matera_params[:discount]
+          },
+          senderComment: matera_params[:description]
+        }
+      }
+
+      p matera_body
+      
+      response = @request.post(@url + ACCOUNT_PATH + account_id + WITHDRAW, matera_body, [matera_body[:totalAmount], account_id, matera_params[:linhaDigitavel]].join(''))
+      MateraModel.new(response['data'] || response['error'])
+    end
   end
 end
